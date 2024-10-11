5be2f447d2a61779e455713f9005ed88eb39cc28
LAB-USR-CHIMBOTE <LAB-USR-CHIMBOTE@grupoutp.net>
2024-10-11 16:12:41 -0500

    ZdenkoTorres

---
+++ ProyectoVehiculo01/src/main/java/com/mycompany/proyectovehiculo01/ProyectoVehiculo01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectovehiculo01;
+
+/**
+ *
+ * @author LAB-USR-CHIMBOTE
+ */
+public class ProyectoVehiculo01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
